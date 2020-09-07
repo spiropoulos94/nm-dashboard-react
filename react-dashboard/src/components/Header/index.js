cf3d3beb5403171ab4b34ccd5eb8149b36fb38d3
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-09-07 10:00:17 +0300

    .

---
+++ react-dashboard/src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import style from "./header.module.scss"
+
+function Header() {
+    return (
+        <div>
+            <div>
+                <img></img>
+                <span>LOGO</span>
+            </div>
+            <h1>Dashboard</h1>
+            <div>
+                <span>LOGOUT</span>
+                <img></img>
+            </div>
+        </div>
+    )
+}
+
+export default Header
