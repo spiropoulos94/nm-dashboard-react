53ee530de13059a112c4587c0e1672855106beff
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-09-07 15:10:21 +0300

    .

--- react-dashboard/src/components/NavBar/NavBar.js
+++ react-dashboard/src/components/NavBar/NavBar.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React,{useState} from "react";
 import "./navbar.scss";
 
 function NavBar() {
+
+  let [hiddenNavBar, setHiddenNavBar] = useState(true)
+
+  function hideNavbar(){
+
+  }
+
+
   return (
-    <div className="navbar">
+    <div>
+    <div className="navbar ">
       <div class="menu-items">
         <a href="#" className="menu-item">Colours</a>
         <a href="#" className="menu-item">Users</a>
       </div>
       <footer class="footer">© 2020 Netmechanics</footer>
+    </div>
+    <button onClick={()=>{console.log("click")}}>Show/Hide Navbar</button>
     </div>
   );
 }
