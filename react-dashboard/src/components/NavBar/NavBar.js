ddcef750dabbd50a435ed355228c419ba25549dc
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-09-07 14:25:48 +0300

    .

--- react-dashboard/src/components/NavBar/NavBar.js
+++ react-dashboard/src/components/NavBar/NavBar.js
@@ -5,8 +5,8 @@ function NavBar() {
   return (
     <div className="navbar">
       <div class="menu-items">
-        <a className="menu-item">Colours</a>
-        <a className="menu-item">Users</a>
+        <a href="#" className="menu-item">Colours</a>
+        <a href="#" className="menu-item">Users</a>
       </div>
       <footer class="footer">© 2020 Netmechanics</footer>
     </div>
