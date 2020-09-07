a13b7b233c1e4f66177b4c453d9fb264c4508629
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-09-07 12:53:08 +0300

    mobile menu

--- react-dashboard/src/components/Header/Header.js
+++ react-dashboard/src/components/Header/Header.js
@@ -1,22 +1,26 @@
-import React from 'react'
-import "./header.scss"
-import LogoPlaceholder from "../../images/logoplaceholder.png"
-import Avatar from "../../images/accountPhoto.png"
+import React from "react";
+import "./header.scss";
+import LogoPlaceholder from "../../images/logoplaceholder.png";
+import Avatar from "../../images/accountPhoto.png";
 
 function Header() {
-    return (
-        <div className="header-wrapper">
-            <div className="header-left">
-                <img className="header-logo" src={LogoPlaceholder} alt="logo" />
-                <span>LOGO</span>
-            </div>
-            <span className="title">Dashboard</span>
-            <div className="header-right">
-                <span>LOGOUT</span>
-                <img src={Avatar} alt="avatar" />
-            </div>
-        </div>
-    )
+  return (
+    <div className="header-wrapper">
+      <div className="header-left">
+        <img className="header-logo" src={LogoPlaceholder} alt="logo" />
+        <span>LOGO</span>
+        <button className="burger-btn">
+        <span className="menu-icon" >&#9776;</span>
+      </button>
+      </div>
+      
+      <span className="title">Dashboard</span>
+      <div className="header-right">
+        <span>LOGOUT</span>
+        <img src={Avatar} alt="avatar" />
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
