ab9c6bdbe7f5b33022193ec94b0285aeeea41f54
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-09-08 12:32:50 +0300

    Update MainView.js

--- react-dashboard/src/components/MainView/MainView.js
+++ react-dashboard/src/components/MainView/MainView.js
@@ -11,9 +11,19 @@ import {
 function MainView() {
     return (
         <div className="main-view-wrapper">
-            <h2 className="heading">Welcome</h2>
-        </div>
+        <Switch>
+          <Route path="/home">
+                <h2>Welcome</h2>
+                </Route>
+          <Route path="/users">
+              <h2>Users Screen</h2>
+          </Route>
+          <Route path="/colours">
+              <h2>Colours Screen</h2>
+          </Route>
+        </Switch>
+    </div>
     )
 }
 
-export default MainView
+export default MainView;
