bf1a527e88dcb76731c98deedcc5fc22d75a10c8
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-09-08 17:44:03 +0300

    Update UsersView.js

--- react-dashboard/src/components/UsersView/UsersView.js
+++ react-dashboard/src/components/UsersView/UsersView.js
@@ -1,34 +1,43 @@
-import React,{useState, useEffect} from 'react';
-import "./usersview.scss"
-
+import React, { useState, useEffect } from "react";
+import "./usersview.scss";
 
 function UsersView() {
+  function getUsers() {
+    fetch("users.json")
+      .then((res) => res.json())
+      .then((response) => setUserData(response.data));
+  }
 
-    function getUsers(){
-        fetch("users.json")
-        .then(res=>res.json())
-        .then(response =>setUserData(response.data))
+  let [userData, setUserData] = useState(null);
+
+  console.log(userData);
+
+  useEffect(() => {
+    getUsers();
+  }, [setUserData]);
+
+  function removeUser() {
+    console.log('user removed')
+    let newArr = userData
+    newArr.pop()
+    setUserData(newArr)
     }
-
-    let [userData, setUserData] = useState(null)
-
-        console.log(userData)
-    useEffect(()=>{
-        getUsers()
-    },[])
-
     
+    
+    
+  
 
-    return (
-      <div>
-        <div className="view-heading">
-        <h2 className="view-title">Users Screen</h2> <button className="delete-btn" >delete</button>
+  return (
+    <div>
+      <div className="view-heading">
+        <h2 className="view-title">Users Screen</h2>{" "}
+        <button onClick={removeUser} className="delete-btn">delete</button>
       </div>
-        <div className="table-responsive">
-        <table id="myTable" className="table" cell-spacing="0" cell-padding="0" >
+      <div className="table-responsive">
+        <table id="myTable" className="table" cell-spacing="0" cell-padding="0">
           <thead className="table-head">
             <tr>
-            <th></th>
+              <th></th>
               <th>ID</th>
               <th>LAST NAME</th>
               <th>FIST NAME</th>
@@ -41,23 +50,29 @@ function UsersView() {
               userData.map((user, index) => {
                 return (
                   <tr key={index}>
-                    <td><label for="radio-btn"><input type='radio' name="radio-btn"></input></label></td>
+                    <td>
+                      <label htmlFor="radio-btn">
+                        <input type="radio" name="radio-btn"></input>
+                      </label>
+                    </td>
                     <td>{user.id}</td>
                     <td>{user["last_name"]}</td>
                     <td>{user["first_name"]}</td>
                     <td>{user["email"]}</td>
-                    <td>{user.avatar.substring(
-                    user.avatar.lastIndexOf("r/") + 2,
-                    user.avatar.lastIndexOf("/128")
-                  )}</td>
+                    <td>
+                      {user.avatar.substring(
+                        user.avatar.lastIndexOf("r/") + 2,
+                        user.avatar.lastIndexOf("/128")
+                      )}
+                    </td>
                   </tr>
                 );
               })}
           </tbody>
         </table>
-        </div>
       </div>
-    );
+    </div>
+  );
 }
 
-export default UsersView
+export default UsersView;
