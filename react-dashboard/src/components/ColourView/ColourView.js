314866f070f649fcd5d2e7b71cdfd674741b8bdb
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-09-08 11:37:54 +0300

    .

--- react-dashboard/src/components/ColourView/ColourView.js
+++ react-dashboard/src/components/ColourView/ColourView.js
@@ -1,34 +1,43 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import "./colourview.scss";
 
-function ColorCard(props){
-    return (
-        <p>this is a color card</p>
-    )
+function ColorCard(props) {
+    let data = props.data
+    console.log(data)
+    
+  return (
+    <div className="color-card">
+      <p className="color-code">{data.color}</p>
+      <div className="color-info">
+        <p className="color-year">{data.year}</p>
+        <p className="color-name">{data.name}</p>
+      </div>
+    </div>
+  );
 }
-
 
 function ColourView() {
+  let [colorData, setColorData] = useState(null);
+  let url = "https://reqres.in/api/products/";
 
-    let [colorData, setColorData] = useState(null)
-    let url = "https://reqres.in/api/products/"
+  useEffect(() => {
+    if (colorData == null) {
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => setColorData(data.data));
+    }
+  }, [colorData]);
 
-    useEffect(() => {
-        if(colorData==null){
-            fetch(url).then(res=>res.json()).then(data=>setColorData(data.data))
-        }
-        
-    }, [colorData])
-
-
-    return (
-        <div className="color-wrapper">
-            Colours rendered here
-            {colorData && colorData.map((color,index) => {
-                return <ColorCard key={index} />
-            })}
-        </div>
-    )
+  return (
+    <div className="color-wrapper">
+      <div className="color-grid">
+      {colorData &&
+        colorData.map((color, index) => {
+          return <ColorCard data={color} key={index} />;
+        })}
+    </div>
+    </div>
+  );
 }
 
-export default ColourView
+export default ColourView;
