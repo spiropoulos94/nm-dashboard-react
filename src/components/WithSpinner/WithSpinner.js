c3107b02542e9a60d37925b1e3e5d0072d166eab
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-09-10 22:36:09 +0300

    TODO

--- src/components/WithSpinner/WithSpinner.js
+++ src/components/WithSpinner/WithSpinner.js
@@ -2,6 +2,10 @@ import React from "react";
 import Spinner from "./../Spinner/Spinner";
 import fetchUrl from "../../utilities/fetchFunction.js";
 
+//TODO
+//how to pass each component endpoint on fetchUrl in order
+//for it to pass fetched data as props on wrapped components
+
 function WithSpinner(Component) {
   return function WithSpinnerComponent({ isLoading, ...props }) {
     if (!isLoading) return <Component {...props} />;
