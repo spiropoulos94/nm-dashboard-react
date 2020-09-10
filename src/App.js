9d3ec65926827afdb9d101c25e8c7b5696e5450a
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-09-10 17:39:58 +0300

    added hoc withspinner

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import ColourView from "./components/ColourView/ColourView.js";
 import UsersView from "./components/UsersView/UsersView.js";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.scss";
-import Spinner from "./components/Spinner/Spinner.js";
 import WithSpinner from "./components/WithSpinner/WithSpinner"
 let ColoursWithSpinner = WithSpinner(ColourView);
 let UsersWithSpinner = WithSpinner(UsersView)
@@ -14,66 +13,48 @@ let UsersWithSpinner = WithSpinner(UsersView)
 function App() {
   let [hiddenNavBar, setHiddenNavBar] = useState(false);
   // let [isLoading, setIsLoading] = useState(true)
-  let [coloursIsLoading, setColoursIsLoading] = useState(true)
-  let [usersIsLoading, setUsersIsLoading] = useState(true)
-
-
-
-  // function checkDataAvailability(){
-  //   fetch("https://reqres.in/api/products/").then(res => {
-  //     if(res.ok){
-  //       setColoursIsLoading(false)
-  //     }
-  //   })
-
-  //   fetch("https://reqres.in/api/users/").then(res => {
-  //     if(res.ok){
-  //       setUsersIsLoading(false)
-  //     }
-  //   })
-  // }
-
-  // checkDataAvailability();
+  let [coloursIsLoading, setColoursIsLoading] = useState(true);
+  let [usersIsLoading, setUsersIsLoading] = useState(true);
 
   function toggleNavbar() {
     setHiddenNavBar(!hiddenNavBar);
   }
 
   function closeNavbar() {
-    if(window.innerWidth < 768){
+    if (window.innerWidth < 768) {
       setHiddenNavBar(true);
     }
   }
 
   //stops scrolling when navbar is open
-  hiddenNavBar 
-    ? document.body.classList.remove('no-scroll')
-    : document.body.classList.add('no-scroll');
+  hiddenNavBar
+    ? document.body.classList.remove("no-scroll")
+    : document.body.classList.add("no-scroll");
 
   window.addEventListener("resize", () => {
     window.innerWidth > 768 && setHiddenNavBar(false);
     window.innerWidth < 768 && setHiddenNavBar(true);
   });
-   
+
   return (
     <Router>
-      <div className="App" >
+      <div className="App">
         <Header closeNavbar={closeNavbar} toggleNavbar={toggleNavbar} />
-        
         <div className="flex-wrapper-main">
-          <NavBar navStatus={hiddenNavBar} setNavStatus={setHiddenNavBar} closeNavbar={closeNavbar} />
+          <NavBar
+            navStatus={hiddenNavBar}
+            setNavStatus={setHiddenNavBar}
+            closeNavbar={closeNavbar}/>
           <div className="data-wrapper">
             <Switch>
               <Route path="/home">
                 <h2 className="view-title">Welcome</h2>
-              </Route> 
+              </Route>
               <Route path="/colours">
-              <ColoursWithSpinner isLoading  />
-                {/* {coloursIsLoading ? <Spinner /> :<ColourView />} */}
+                <ColoursWithSpinner isLoading={coloursIsLoading} />
               </Route>
               <Route path="/users">
-              <UsersWithSpinner  isLoading />
-               {/* {usersIsLoading ? <Spinner /> :<UsersView />} */}
+                <UsersWithSpinner isLoading={usersIsLoading} />
               </Route>
             </Switch>
           </div>
