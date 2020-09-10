b1b2fe924fe1ced375ac219bab189412bb5333a5
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-09-10 09:42:29 +0300

    added spinner on whole page

--- react-dashboard/src/components/UsersView/UsersView.js
+++ react-dashboard/src/components/UsersView/UsersView.js
@@ -2,11 +2,10 @@ import React, { useState, useEffect } from "react";
 import "./usersview.scss";
 import Spinner from "./../Spinner/Spinner.js";
 
-
 function UsersView() {
   let url = "https://reqres.in/api/users/";
   let [users, setUsers] = useState(null);
-  let [isLoading, setIsLoading] = useState(true)
+  let [isLoading, setIsLoading] = useState(true);
   let [disabledBtn, setDisabledBtn] = useState(true);
 
   //GETTING USERS DATA
@@ -23,32 +22,27 @@ function UsersView() {
           if (keyA > keyB) return +1;
           return 0;
         });
-        
+
         setUsers(response.data);
-        
+
         sessionStorage.setItem("users", JSON.stringify(response.data));
       })
-      .catch(err => {
-        console.log(err)
-        setIsLoading(true)
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(true);
       });
   }
 
-  //HYDRATING USERS
-  // function updateUsers() {
-  //   setUserData(JSON.parse(sessionStorage.getItem("users")));
-  // }
-
   //IF SESSION STORAGE DATA ARE AVAILABLE USE THEM, IF NOT GET DATA
   useEffect(() => {
-    setIsLoading(false)
+    setIsLoading(false);
     if (!JSON.parse(sessionStorage.getItem("users"))) {
       getUsers();
     } else {
       // updateUsers();
-      setUsers(JSON.parse(sessionStorage.getItem("users")))
+      setUsers(JSON.parse(sessionStorage.getItem("users")));
     }
-  }, [url]);
+  }, []);
 
   //REMOVE SELECTED USER
   function removeUser() {
@@ -78,62 +72,71 @@ function UsersView() {
 
   return (
     <>
-    {isLoading ? <Spinner/> : <div>
-      <div className="view-heading">
-        <h2 className="view-title">Users Screen</h2>{" "}
-        <button
-          onClick={removeUser}
-          className="delete-btn"
-          disabled={disabledBtn}
-        >
-          delete
-        </button>
-      </div>
-      <div className="table-responsive">
-        <table id="myTable" className="table" cell-spacing="0" cell-padding="0">
-          <thead className="table-head">
-            <tr>
-              <th></th>
-              <th>ID</th>
-              <th>LAST NAME</th>
-              <th>FIST NAME</th>
-              <th>EMAIL</th>
-              <th>AVATAR</th>
-            </tr>
-          </thead>
-          <tbody className="tbody">
-            {users &&
-              users.map((user) => {
-                let {avatar} = user;
-                return (
-                  <tr key={user.id}>
-                    <td>
-                      {/* <label   */}
-                        <input
-                          className="radio-btn"
-                          type="radio"
-                          name="radio-btn"
-                          onChange={toggleDeleteBtn}>
-                        </input>
-                      {/* </label> */}
-                    </td>
-                    <td>{user.id}</td>
-                    <td>{user["last_name"]}</td>
-                    <td>{user["first_name"]}</td>
-                    <td>{user["email"]}</td>
-                    <td>
-                      {avatar.substring(
-                        avatar.lastIndexOf("r/") + 2,
-                        avatar.lastIndexOf("/128")
-                      )}
-                    </td>
-                  </tr>
-                );
-              })}
-          </tbody>
-        </table>
-      </div>
-    </div>}
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div>
+          <div className="view-heading">
+            <h2 className="view-title">Users Screen</h2>{" "}
+            <button
+              onClick={removeUser}
+              className="delete-btn"
+              disabled={disabledBtn}
+            >
+              delete
+            </button>
+          </div>
+          <div className="table-responsive">
+            <table
+              id="myTable"
+              className="table"
+              cell-spacing="0"
+              cell-padding="0"
+            >
+              <thead className="table-head">
+                <tr>
+                  <th></th>
+                  <th>ID</th>
+                  <th>LAST NAME</th>
+                  <th>FIST NAME</th>
+                  <th>EMAIL</th>
+                  <th>AVATAR</th>
+                </tr>
+              </thead>
+              <tbody className="tbody">
+                {users &&
+                  users.map((user) => {
+                    let { avatar } = user;
+                    return (
+                      <tr key={user.id}>
+                        <td>
+                          <label>
+                            <input
+                              className="radio-btn"
+                              type="radio"
+                              name="radio-btn"
+                              onChange={toggleDeleteBtn}
+                            />
+                          </label>
+                        </td>
+                        <td>{user.id}</td>
+                        <td>{user["last_name"]}</td>
+                        <td>{user["first_name"]}</td>
+                        <td>{user["email"]}</td>
+                        <td>
+                          {avatar.substring(
+                            avatar.lastIndexOf("r/") + 2,
+                            avatar.lastIndexOf("/128")
+                          )}
+                        </td>
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
     </>
   );
 }
