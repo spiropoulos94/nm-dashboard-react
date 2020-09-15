6a72394f9457aa7ddb02f02cf3fd9ecdfddb5734
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-09-15 15:10:19 +0300

    Function updateStateOnFetch applied on usersViewContainer

--- src/components/UsersView/UsersViewContainer.jsx
+++ src/components/UsersView/UsersViewContainer.jsx
@@ -6,18 +6,21 @@ import Spinner from "../Spinner/Spinner";
 function UsersViewContainer({ url, setIsLoading, users, setUsers }) {
   let [_isLoading, _setIsLoading] = useState(users===null);
 
+
+  function updateStateOnFetch(response) {
+    const _data = response && response.hasOwnProperty('data') && response['data'] || [];
+    setUsers(_data);
+    _setIsLoading(false);
+    setIsLoading(false);
+  }
+
   useEffect(() => {
     if (!users) {
       fetchUrl(url).then((response) => {
         console.log("Users endpoint request was made!");
-        if (response.hasOwnProperty("data")) {
-          setUsers(response.data);
-        }
+        updateStateOnFetch(response)
       });
     } 
-    _setIsLoading(false)
-    setIsLoading(_isLoading)
-
   }, []);
 
   if (_isLoading) return <Spinner />;
