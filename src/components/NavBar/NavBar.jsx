7dae9526434b591c2eff71be0cd777b27e28fd02
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-09-18 15:57:18 +0300

    .

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,22 +4,8 @@ import "./NavBar.scss";
 
 function NavBar({hiddenNavBar, updateNavBar}) {
 
-    let navbar = document.querySelector(".navbar")
-    console.log(navbar)
-
-
-     // useEffect(()=>{
-     //   navbar && navbar.addEventListener('transitionend', (e) => {
-     //     if(navbar.classList.contains("hidden")) {
-     //       console.log("navbar is hidden")
-     //       navbar.style.position = "unset"
-     //     }
-     //   })
-     // },[navbar])
-
 
     return (
-
         <div className={hiddenNavBar ? "navbar hidden" : "navbar"}>
             <div className="menu-items">
                 <Link to="/colours" className="menu-item" onClick={() => updateNavBar(true)}>
