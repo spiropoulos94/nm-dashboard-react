bb7b102f27132ce24389e89afe71bcdd20b18abb
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-09-18 11:51:42 +0300

    change display of navbar on transition end

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,38 +1,36 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import "./NavBar.scss";
 
 function NavBar({hiddenNavBar, updateNavBar}) {
 
-  let navbar = document.querySelector(".navbar")
-  console.log(navbar)
+    let navbar = document.querySelector(".navbar")
+    console.log(navbar)
 
-  navbar && navbar.addEventListener('transitionend', (e)=>{
-    if(navbar.classList.contains(" hidden")){
-      navbar.style.display ="none";
-    }
-    else{
-      navbar.style.display="flex";
-    }
-
-  })
+    navbar && navbar.addEventListener('transitionend', (e) => {
+        if (navbar.classList.contains(" hidden")) {
+            navbar.parentElement.style.display = "none";
+        } else {
+            navbar.style.display = "flex";
+        }
+    })
 
 
-  return (
-    <div>
-      <div className={hiddenNavBar ? "navbar hidden" : "navbar"}>
-        <div className="menu-items">
-          <Link to="/colours" className="menu-item" onClick={() => updateNavBar(true)}>
-            Colours
-          </Link>
-          <Link to="/users" className="menu-item" onClick={() => updateNavBar(true)}>
-            Users
-          </Link>
+    return (
+
+        <div className={hiddenNavBar ? "navbar hidden" : "navbar"}>
+            <div className="menu-items">
+                <Link to="/colours" className="menu-item" onClick={() => updateNavBar(true)}>
+                    Colours
+                </Link>
+                <Link to="/users" className="menu-item" onClick={() => updateNavBar(true)}>
+                    Users
+                </Link>
+            </div>
+            <footer className="footer">© 2020 Netmechanics</footer>
         </div>
-        <footer className="footer">© 2020 Netmechanics</footer>
-      </div>
-    </div>
-  ); 
+
+    );
 }
 
 export default NavBar;
